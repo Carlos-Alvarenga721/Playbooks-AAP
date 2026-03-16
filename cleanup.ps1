ca7a593d3f51c49767a0d7bd6b7f8a56530848a5
Carlos Alvarenga <c.alvarenga.c7@gmail.com>
2026-03-16 17:41:57 -0600

    Subiendo archivo para inventario de Github

--- cleanup.ps1
+++
@@ -1,4 +0,0 @@
-$path = 'c:\Users\ialva\Desktop\UDB CICLOS\AAP PLAYBOOKS'
-$exclude = @('.git', '.github', 'cleanup.ps1')
-Get-ChildItem -Path $path -Force | Where-Object { $_.Name -notin $exclude } | Remove-Item -Recurse -Force
-Write-Host 'Cleanup done'
--- verify.ps1
+++
@@ -1,1 +0,0 @@
-Get-ChildItem -Path 'c:\Users\ialva\Desktop\UDB CICLOS\AAP PLAYBOOKS\aap-poc' -Recurse | Select-Object FullName
